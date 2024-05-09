1165a0727f3734581838086ace8e0ea7ba24288f
Lahotiharsh <149718078+Lahotiharsh@users.noreply.github.com>
2024-05-09 08:28:50 -0700

    book schema created

--- server/models/bookmodel.js
+++ server/models/bookmodel.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-// export const book = mongoose.model("book",{ name:String , author:String , pages: Number }); // better to use schema
 const bookSchema = mongoose.Schema(
     {
         title:{
@@ -21,8 +20,6 @@ const bookSchema = mongoose.Schema(
 
 )
 
-// module.exports = mongoose.model("book", )
 
 export const Book = mongoose.model("book", bookSchema);   // cat will become the name of  collection in test database
-// ab iska route(get/post) banana in server file isko import karke..
 
